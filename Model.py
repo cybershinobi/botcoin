33741b8ed654e8ed421e0651359a717bfffe1540
Ricardo Araujo <ricarrrdoluigi@gmail.com>
2017-10-24 02:56:01 -0200

    Added command line arguments

--- Model.py
+++ Model.py
@@ -1,17 +1,59 @@
 import time
 import datetime
-from poloniex import Poloniex
-import os
+import poloniex
+import os, sys, getopt
 
+k_key = '2RABZPOL-EFN4VC8V-3VAIR7CI-8BJWZM4R'
+k_secret = 'a28e3bc7d61f48d1292397eb78c62a1800be88cdeabfd0172cf713dd2c90452988f37fe0d4417510c422a6c792effe75cd8ccf2b7396b500b35b27523f7a12b2'
+
+help_msg = '-p --period\t\tthe period that the bot wait for'
 
 
 class Model():
-    def __init__(self, period, currency, key, secret):
-        self.period = period
-        self.conn = Poloniex(key, secret)
-        self.currency = currency
+    def __init__(self,argv):
+        self.conn = poloniex.Poloniex(k_key, k_secret)
         self.prices = []
-        self.movingAvg = 0
+        self.currency = 0
+        self.update = 10
+        self.period = 0
+        self.starttime = 0
+        self.endtime = 0
+        self.perVal = [300,900,1800,7200,14400,86400]
+        self.main(argv)
+
+    def main(self,argv):   
+        
+        try:
+            opts, args = getopt.getopt(argv,"hp:c:t:e:s:",["help","period=","currency=","starttime=","endtime=","strategy="])
+        except getopt.GetoptError as err:
+            print(str(err))
+            sys.exit(1)
+
+        for opt, arg in opts:
+            if opt in ('-h','--help'):
+                print(help_msg)
+            elif opt in ('-p','--period'):
+                if (int(arg) in self.perVal):
+                    self.period = int(arg)
+                else:
+                    print('Not allowed period! Poloniex requires 300,900,1800,7200,14400,86400.')    
+                    sys.exit(1)
+            elif opt in ('-c','--currency'):
+                self.currency = arg
+            elif opt in ('-t','--starttime'):
+                self.starttime  = arg
+            elif opt in ('-e','--endtime'):
+                self.endtime = arg    
+            elif opt in ('-s','--strategy'):
+                if (arg == 'chart'):
+                    self.chartData()
+                elif(arg == 'bot'):
+                    self.bot()
+                elif (arg == 'history'):
+                    self.tradeHistory()                    
+                else:
+                    print('Chose a strategy. Type --help to see the suported strategies.')        
+
 
     def lastPrice(self):
         ticker = self.conn.returnTicker()
@@ -23,27 +65,38 @@ class Model():
         print(orders)
 
     def tradeHistory(self):
-        print(self.conn.returnMarketTradeHistory(self.currency)[0])
+        print(self.conn.marketTradeHist(self.currency,1405699200,1405749900)[:]) #teste
 
-    def toChart(self):
-        file = open('prices.txt', 'w')
+    def chartData(self):
+        if (self.starttime):
+            graph = self.conn.returnChartData(str(self.currency),self.period,self.starttime,self.endtime)
+            getChart = graph.pop(0)
+            returnedChart = getChart['weightedAverage']
+        print(returnedChart)    
+
+    def bot(self):
+        #file = open('prices.txt', 'w')
         while True:
             try:
                 currentPrice = self.conn.returnTicker()
                 lastPrice = currentPrice[self.currency]['last']
-                self.prices.append(float(lastPrice))
-                file.write('{:%H:%M:%S},{}\n'.format(datetime.datetime.now(),lastPrice))
-                #print('{:%d-%m-%Y %H:%M:%S}'.format(datetime.datetime.now()) + ' Currency Pair: ' + self.currency + ' Price: '+str(lastPrice)+' Moving Average: '+str(self.movingAvg))
+                #self.prices.append(float(lastPrice))
+                #file.write('{:%H:%M:%S},{}\n'.format(datetime.datetime.now(),lastPrice))
+                print('{:%d-%m-%Y %H:%M:%S}'.format(datetime.datetime.now()) + ' ' + self.currency + ': '+str(lastPrice))
                 #self.movingAvg = sum(self.prices) / len(self.prices)
-                file.flush()
-                os.fsync(file.fileno())
+                #file.flush()
+                #os.fsync(file.fileno())
 
-                time.sleep(self.period)
+                time.sleep(int(self.update))
             except KeyboardInterrupt:
-                file.close()
+                print('\nUser interruption... Shutting down bot...')
+                time.sleep(2)
+                #file.close()
                 break
 
 
 if __name__ == '__main__':
-    I = Model(10,'BTC_XMR','your-key-here','your-secret-here')
-    I.toChart()
+    Model(sys.argv[1:])
+    #I.main(sys.argv[1:])
+    #I = Model(10,'BTC_XMR','2RABZPOL-EFN4VC8V-3VAIR7CI-8BJWZM4R','a28e3bc7d61f48d1292397eb78c62a1800be88cdeabfd0172cf713dd2c90452988f37fe0d4417510c422a6c792effe75cd8ccf2b7396b500b35b27523f7a12b2')
+    #I.toChart()
